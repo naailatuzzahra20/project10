c6110da798abbacc4f15353fddf3389d070acd3a
nai <nayylaawn@gmail.com>
2024-11-14 11:58:43 +0700

    perubahan awal

---
+++ function01.php
@@ -0,0 +1,10 @@
+<?php
+function hitunglingkaran($jariJari) {
+    return pi() * pow($jariJari, 2);  // pi() = 3.14159..., pow() = pangkat
+}
+function hitungluassegitiga($alas,$tinggi(5*8\2) {
+    return $alas*$tinggi\2; 
+}
+
+echo "Luas luas segitiga: ".hitungluassegitiga(5,8\2);  // Output: Luas lingkaran: 153.9380400259
+?>
